a4509ef72bbf60ddd1fab835ea5832267bffec2d
Shofi Ahmad <60640061+shofiahmad06@users.noreply.github.com>
2020-07-06 19:18:40 +0600

    Update CanvasFactory.php

--- Final_Submission/dompdf/src/CanvasFactory.php
+++ Final_Submission/dompdf/src/CanvasFactory.php
@@ -1,37 +1,16 @@
 <?php
-/**
- * @package dompdf
- * @link    http://dompdf.github.com/
- * @author  Benj Carson <benjcarson@digitaljunkies.ca>
- * @license http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License
- */
+
 namespace Dompdf;
 
-/**
- * Create canvas instances
- *
- * The canvas factory creates canvas instances based on the
- * availability of rendering backends and config options.
- *
- * @package dompdf
- */
+
 class CanvasFactory
 {
-    /**
-     * Constructor is private: this is a static class
-     */
+
     private function __construct()
     {
     }
 
-    /**
-     * @param Dompdf $dompdf
-     * @param string|array $paper
-     * @param string $orientation
-     * @param string $class
-     *
-     * @return Canvas
-     */
+   
     static function get_instance(Dompdf $dompdf, $paper = null, $orientation = null, $class = null)
     {
         $backend = strtolower($dompdf->getOptions()->getPdfBackend());
