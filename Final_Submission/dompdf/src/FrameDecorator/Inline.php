58e7a2fe98ad6fae13a610797b8840ed1bbebce4
Shofi Ahmad <60640061+shofiahmad06@users.noreply.github.com>
2020-07-06 20:09:59 +0600

    Update Inline.php

--- Final_Submission/dompdf/src/FrameDecorator/Inline.php
+++ Final_Submission/dompdf/src/FrameDecorator/Inline.php
@@ -1,11 +1,5 @@
 <?php
-/**
- * @package dompdf
- * @link    http://dompdf.github.com/
- * @author  Benj Carson <benjcarson@digitaljunkies.ca>
- * @author  Helmut Tischer <htischer@weihenstephan.org>
- * @license http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License
- */
+
 namespace Dompdf\FrameDecorator;
 
 use DOMElement;
@@ -13,30 +7,17 @@ use Dompdf\Dompdf;
 use Dompdf\Frame;
 use Dompdf\Exception;
 
-/**
- * Decorates frames for inline layout
- *
- * @access  private
- * @package dompdf
- */
+
 class Inline extends AbstractFrameDecorator
 {
 
-    /**
-     * Inline constructor.
-     * @param Frame $frame
-     * @param Dompdf $dompdf
-     */
+
     function __construct(Frame $frame, Dompdf $dompdf)
     {
         parent::__construct($frame, $dompdf);
     }
 
-    /**
-     * @param Frame|null $frame
-     * @param bool $force_pagebreak
-     * @throws Exception
-     */
+
     function split(Frame $frame = null, $force_pagebreak = false)
     {
         if (is_null($frame)) {
@@ -75,9 +56,7 @@ class Inline extends AbstractFrameDecorator
         $style->padding_left = 0;
         $style->border_left_width = 0;
 
-        //On continuation of inline element on next line,
-        //don't repeat non-vertically repeatble background images
-        //See e.g. in testcase image_variants, long desriptions
+
         if (($url = $style->background_image) && $url !== "none"
             && ($repeat = $style->background_repeat) && $repeat !== "repeat" && $repeat !== "repeat-y"
         ) {
