54193717c8d2ec184f08a92b702aa00eddc7e118
Shofi Ahmad <60640061+shofiahmad06@users.noreply.github.com>
2020-07-06 19:37:22 +0600

    Update AbstractPositioner.php

--- Final_Submission/dompdf/src/Positioner/AbstractPositioner.php
+++ Final_Submission/dompdf/src/Positioner/AbstractPositioner.php
@@ -1,38 +1,18 @@
 <?php
-/**
- * @package dompdf
- * @link    http://dompdf.github.com/
- * @author  Benj Carson <benjcarson@digitaljunkies.ca>
- * @license http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License
- */
+
 
 namespace Dompdf\Positioner;
 
 use Dompdf\FrameDecorator\AbstractFrameDecorator;
 
-/**
- * Base AbstractPositioner class
- *
- * Defines postioner interface
- *
- * @access  private
- * @package dompdf
- */
+
 abstract class AbstractPositioner
 {
 
-    /**
-     * @param AbstractFrameDecorator $frame
-     * @return mixed
-     */
+
     abstract function position(AbstractFrameDecorator $frame);
 
-    /**
-     * @param AbstractFrameDecorator $frame
-     * @param $offset_x
-     * @param $offset_y
-     * @param bool $ignore_self
-     */
+
     function move(AbstractFrameDecorator $frame, $offset_x, $offset_y, $ignore_self = false)
     {
         list($x, $y) = $frame->get_position();
