9306a278d4fd90bc05b5bcc8e23d1deba912ed8e
Shofi Ahmad <60640061+shofiahmad06@users.noreply.github.com>
2020-07-06 19:35:12 +0600

    Update Inline.php

--- Final_Submission/dompdf/src/Positioner/Inline.php
+++ Final_Submission/dompdf/src/Positioner/Inline.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * @package dompdf
- * @link    http://dompdf.github.com/
- * @author  Benj Carson <benjcarson@digitaljunkies.ca>
- * @license http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License
- */
+
 
 namespace Dompdf\Positioner;
 
@@ -13,33 +8,17 @@ use Dompdf\FrameDecorator\Inline as InlineFrameDecorator;
 use Dompdf\FrameDecorator\Block as BlockFrameDecorator;
 use Dompdf\Exception;
 
-/**
- * Positions inline frames
- *
- * @package dompdf
- */
+
 class Inline extends AbstractPositioner
 {
 
-    /**
-     * @param AbstractFrameDecorator $frame
-     * @throws Exception
-     */
+
     function position(AbstractFrameDecorator $frame)
     {
-        /**
-         * Find our nearest block level parent and access its lines property.
-         * @var BlockFrameDecorator
-         */
+
         $p = $frame->find_block_parent();
 
-        // Debugging code:
 
-        // Helpers::pre_r("\nPositioning:");
-        // Helpers::pre_r("Me: " . $frame->get_node()->nodeName . " (" . spl_object_hash($frame->get_node()) . ")");
-        // Helpers::pre_r("Parent: " . $p->get_node()->nodeName . " (" . spl_object_hash($p->get_node()) . ")");
-
-        // End debugging
 
         if (!$p) {
             throw new Exception("No block-level parent found.  Not good.");
@@ -50,7 +29,7 @@ class Inline extends AbstractPositioner
         $cb = $f->get_containing_block();
         $line = $p->get_current_line_box();
 
-        // Skip the page break if in a fixed position element
+       
         $is_fixed = false;
         while ($f = $f->get_parent()) {
             if ($f->get_style()->position === "fixed") {
@@ -67,7 +46,7 @@ class Inline extends AbstractPositioner
         ) {
             $min_max = $f->get_reflower()->get_min_max_width();
 
-            // If the frame doesn't fit in the current line, a line break occurs
+          
             if ($min_max["min"] > ($cb["w"] - $line->left - $line->w - $line->right)) {
                 $p->add_line();
             }
