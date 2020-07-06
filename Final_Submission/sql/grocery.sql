ed7e75477a7a9e1e0e12bdb9fafccf031d9f6269
Shofi Ahmad <60640061+shofiahmad06@users.noreply.github.com>
2020-07-06 21:29:55 +0600

    Update grocery.sql

--- Final_Submission/sql/grocery.sql
+++ Final_Submission/sql/grocery.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.7.4
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Oct 16, 2018 at 04:26 PM
--- Server version: 10.1.28-MariaDB
--- PHP Version: 5.6.32
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
