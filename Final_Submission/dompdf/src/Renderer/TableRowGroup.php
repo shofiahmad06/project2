94edef83f575e0e4c93e60ca63df80936e864c50
Shofi Ahmad <60640061+shofiahmad06@users.noreply.github.com>
2020-07-06 19:21:33 +0600

    Update TableRowGroup.php

--- Final_Submission/dompdf/src/Renderer/TableRowGroup.php
+++ Final_Submission/dompdf/src/Renderer/TableRowGroup.php
@@ -1,25 +1,14 @@
 <?php
-/**
- * @package dompdf
- * @link    http://dompdf.github.com/
- * @author  Benj Carson <benjcarson@digitaljunkies.ca>
- * @license http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License
- */
+
 namespace Dompdf\Renderer;
 
 use Dompdf\Frame;
 
-/**
- * Renders block frames
- *
- * @package dompdf
- */
+
 class TableRowGroup extends Block
 {
 
-    /**
-     * @param Frame $frame
-     */
+  
     function render(Frame $frame)
     {
         $style = $frame->get_style();
