9fd3341f20f95047f4d909e78cf146df2b39235e
Shofi Ahmad <60640061+shofiahmad06@users.noreply.github.com>
2020-07-06 18:31:36 +0600

    Update DeliveryDetails.php

--- Final_Submission/DeliveryDetails.php
+++ Final_Submission/DeliveryDetails.php
@@ -43,7 +43,7 @@ $search_value = "";
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Noodles&Canned</title>
+	<title>Samsung</title>
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
