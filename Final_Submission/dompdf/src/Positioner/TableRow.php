20e2d1211858e5a01bae4b80cd750ecba89eae9f
Shofi Ahmad <60640061+shofiahmad06@users.noreply.github.com>
2020-07-06 19:32:39 +0600

    Update TableRow.php

--- Final_Submission/dompdf/src/Positioner/TableRow.php
+++ Final_Submission/dompdf/src/Positioner/TableRow.php
@@ -1,26 +1,15 @@
 <?php
-/**
- * @package dompdf
- * @link    http://dompdf.github.com/
- * @author  Benj Carson <benjcarson@digitaljunkies.ca>
- * @license http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License
- */
+
 
 namespace Dompdf\Positioner;
 
 use Dompdf\FrameDecorator\AbstractFrameDecorator;
 
-/**
- * Positions table rows
- *
- * @package dompdf
- */
+
 class TableRow extends AbstractPositioner
 {
 
-    /**
-     * @param AbstractFrameDecorator $frame
-     */
+ 
     function position(AbstractFrameDecorator $frame)
     {
         $cb = $frame->get_containing_block();
